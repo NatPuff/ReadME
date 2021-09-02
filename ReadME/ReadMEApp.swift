40f40174bdcd17a18b90985528819b84d6d82c9b
Cesar Pacheco <cesarp102104@gmail.com>
2021-09-02 08:49:30 -0500

    Initial Commit

---
+++ ReadME/ReadMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMEApp.swift
+//  ReadME
+//
+//  Created by AMStudent on 9/2/21.
+//
+
+import SwiftUI
+
+@main
+struct ReadMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
