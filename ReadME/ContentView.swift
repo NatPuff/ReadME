a518db803528c20842e49f96ab6a0c712bf336e0
NatPuff <cesarp102104@gmail.com>
2021-10-08 09:05:38 -0500

    Finished

--- ReadME/ContentView.swift
+++ ReadME/ContentView.swift
@@ -7,15 +7,61 @@
 
 import SwiftUI
 
+
+
+
 struct ContentView: View {
+    @State var library = Library()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       
+        NavigationView {
+        List(library.sortedBooks) { book in
+            BookRow(book: book, image: $library.uiImages[book]
+            )
+        }
+        .navigationBarTitle("My Library")
+        }
     }
 }
-
+struct BookRow: View {
+    @ObservedObject var book: Book
+    @Binding var image: UIImage?
+    
+    var body: some View {
+       NavigationLink(
+        destination: DetailView(book: book, image: $image)
+       ) {
+        
+        HStack {
+            Book.Image(
+                uiImage: image,
+                title: book.title,
+                size: 80,
+                cornerRadius: 12)
+            
+            VStack {
+                TitleAndAuthorStack(book: book, titleFont: .title2, authorFont: .title3)
+                Spacer()
+                Text(book.microReview)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            .lineLimit(1)
+            Spacer()
+            BookmarkButton(book: book)
+        }
+        .padding(.vertical, 8)
+      } 
+  }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewedInAllColorSchemes
     }
 }
+
+
+
+
+
---
+++ ReadME/views/DetailView.swift
@@ -0,0 +1,68 @@
+//
+//  DetailView.swift
+//  ReadME
+//
+//  Created by AMStudent on 9/7/21.
+//
+import class PhotosUI.PHPickerViewController
+import SwiftUI
+
+struct DetailView: View {
+    @ObservedObject var book: Book
+    @Binding var image: UIImage?
+    @State var showingImagePicker = false
+    @State var showingAlert = false
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(spacing: 16) {
+                BookmarkButton(book: book)
+                
+                TitleAndAuthorStack(
+                    book: book,
+                    titleFont: .title,
+                    authorFont: .title2)
+            }
+            VStack {
+                Divider()
+                    .padding()
+                TextField("Review...", text: $book.microReview)
+                Book.Image(
+                    uiImage: image, 
+                    title: book.title,
+                    cornerRadius: 16)
+                    .scaledToFit()
+                HStack {
+                    Button("Update Image") {
+                        showingImagePicker = true
+                    }
+                    .padding()
+                    Button("Delete Image") {
+                        if image != nil{
+                            showingAlert = true
+                        } else { showingAlert = false
+                        }
+                    }
+                }
+            }
+            Spacer()
+        }
+        .padding()
+        .sheet(isPresented: $showingImagePicker) {
+            PHPickerViewController.View(image: $image)
+        }
+        .alert(isPresented: $showingAlert) {
+            .init(title: .init("Delete image for \(book.title)?"), primaryButton: .destructive(.init("Delete")) {
+                image = nil
+            },
+            secondaryButton: .cancel())
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(book: .init(), image: .constant(nil))
+            .previewedInAllColorSchemes
+    }
+}
+
